004f59d8e681aeb3edfe7f1c428e455b29afbbc5
Alexander Dimitrov <a.dimitrov93@gmail.com>
2022-05-15 10:07:54 +0300

    Add OTA. Switch to controlling impulse relays.

--- car-charger-controller/include/charging-session.h
+++ car-charger-controller/include/charging-session.h
@@ -6,6 +6,7 @@
 
 enum ChargingSessionStatus
 {
+    notStarted,
     waiting,
     inProgress,
     ended,
--- car-charger-controller/src/main.cpp
+++ car-charger-controller/src/main.cpp
@@ -3,6 +3,8 @@
 #include <AsyncJson.h>
 #include <AsyncWebSocket.h>
 #include <time.h>
+#include <ArduinoOTA.h>
+#include <SPIFFS.h>
 #include "wifi-config.h"
 #include "charging-session.h"
 
@@ -14,6 +16,7 @@
 AsyncWebServer server(80);
 
 ChargingSession* currentChargingSession;
+ChargingSessionStatus lastChargingStatus = notStarted;
 
 void connectToWiFi(WiFiConfig wifiConfig)
 {
@@ -32,11 +35,17 @@ void connectToWiFi(WiFiConfig wifiConfig)
 
 void runWebServer()
 {
+    server.on("/status", HTTP_GET, [](AsyncWebServerRequest *request)
+    {
+        request->send(200, "text/plain", "Up and running.");
+    });
+
     server.addHandler(new AsyncCallbackJsonWebHandler("/car-charging/start", [](AsyncWebServerRequest *request, JsonVariant &json)
     {
         if (json["offset"].isNull() || json["duration"].isNull())
         {
             request->send(400);
+            return;
         }
         
         time_t currentTime = time(NULL);
@@ -120,6 +129,41 @@ void runWebServer()
     server.begin();
 }
 
+void setupOta()
+{
+    ArduinoOTA
+    .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+            type = "sketch";
+        else // U_SPIFFS
+            type = "filesystem";
+
+        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+        Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+        Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+    ArduinoOTA.begin();
+
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
 void setup()
 {
     Serial.begin(115200);
@@ -137,20 +181,33 @@ void setup()
     WiFiConfig wifiConfig = readWifiConfig();
     connectToWiFi(wifiConfig);
 
+    setupOta();
+
     runWebServer();
+}
+
+void toggle(int port)
+{
+    digitalWrite(CAR_CHARGING_PORT, LOW);
+    delay(250);
+    digitalWrite(CAR_CHARGING_PORT, HIGH);
 }
 
 void loop()
 {
+    ArduinoOTA.handle();
+
     if (currentChargingSession != nullptr)
     {
         ChargingSessionStatus status = currentChargingSession->getStatus();
-        if (status == inProgress)
+        if (status != lastChargingStatus)
         {
-            digitalWrite(CAR_CHARGING_PORT, LOW);
-            return;
+            if (status == inProgress || lastChargingStatus == inProgress)
+            {
+                toggle(CAR_CHARGING_PORT);
+            }
+
+            lastChargingStatus = status;
         }
     }
-
-    digitalWrite(CAR_CHARGING_PORT, HIGH);
 }
